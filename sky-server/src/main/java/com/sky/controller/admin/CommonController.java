cd1544f59b97c6704151a59edea4e392f0f5ce36
embrace <embrace-@outlook.com>
2023-08-14 01:19:53 +0800

    新增菜品业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,63 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 通用接口
+ */
+
+@RestController
+@RequestMapping("/admin/common")
+@Api("通用接口")
+@Slf4j
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
+    public Result<String> upload(MultipartFile file) {
+        log.info("文件上传：{}", file);
+
+        try {
+            // 原始文件名
+            String originalFilename = file.getOriginalFilename();
+
+            // 构造新文件名称
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String fileName = UUID.randomUUID() + extension;
+
+            // 存储路径
+            String bucketName = aliOssUtil.getBucketName();
+
+            File dir = new File(bucketName);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            // 保存文件
+            file.transferTo(new File(bucketName, fileName));
+
+            return Result.success(bucketName + fileName);
+        } catch (IOException e) {
+            log.error("文件上传失败：{}", e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+
+}
